2abffee0739e59cac4ed1b6dbbf2152d6c736dd8
Willian Ribeiro <willian.ribeiro@gauge.com.br>
2016-12-07 18:06:18 -0200

    Add CustomInput component

---
+++ src/components/CustomInput.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+class CustomInput extends Component {
+  render() {
+    return (
+      <div className="pure-control-group">
+        <label
+          htmlFor={ this.props.id } >
+            { this.props.label }
+        </label>
+        <input
+          id={ this.props.id }
+          type={ this.props.type }
+          name={ this.props.name }
+          value={ this.props.value }
+          onChange={ this.props.onChange} />
+      </div>
+    );
+  }
+}
+
+export default CustomInput;
