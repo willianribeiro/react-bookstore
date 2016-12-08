d21648db115ebbf65732ce3921a5f5602102d5cc
Willian Ribeiro <willian.ribeiro@gauge.com.br>
2016-12-08 17:54:44 -0200

    Improve code style

--- src/components/CustomInput.js
+++ src/components/CustomInput.js
@@ -22,8 +22,7 @@ class CustomInput extends Component {
   render() {
     return (
       <div className="pure-control-group">
-        <label
-          htmlFor={ this.props.id } >
+        <label htmlFor={ this.props.id } >
             { this.props.label }
         </label>
         <input
